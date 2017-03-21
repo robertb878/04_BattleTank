c24dea04489e00f0b4f51d125a184479d11c5821
robertb878 <robert@vanahmar.com>
2017-03-21 11:16:29 +0200

    Initial Unreal Commit

---
+++ BattelTank/Source/BattelTank/BattelTankGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BattelTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTELTANK_API ABattelTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
